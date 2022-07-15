b0c083ad92f68fa2e7f4bb37cbb553c9d705f03b
Praveen <Praveen@LAPTOP-RQMIKUIO>
2022-07-15 18:07:04 +0530

    added campaign module

---
+++ src/test/java/production/Campaign.java
@@ -0,0 +1,14 @@
+package production;
+
+import org.testng.annotations.Test;
+
+public class Campaign {
+	@Test
+	public void createContactTest() {
+		System.out.println("createContactTest executed");
+	}
+	@Test
+	public void createOrganizationTest() {
+		System.out.println("createOrganizationTest executed");
+	}
+}
